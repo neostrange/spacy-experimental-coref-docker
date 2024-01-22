3d85feae6326d924fe31ee9428bff5ac61c25984
neostrange <ali.hur57@gmail.com>
2024-01-22 12:02:49 +0800

    tokenizer updated

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
 import spacy
 from flask import Flask, request, jsonify
+from spacy.lang.char_classes import ALPHA, ALPHA_LOWER, ALPHA_UPPER
+from spacy.lang.char_classes import CONCAT_QUOTES, LIST_ELLIPSES, LIST_ICONS
+from spacy.util import compile_infix_regex
 
 
 app = Flask(__name__)
 nlp = spacy.load("en_coreference_web_trf")
 #doc = nlp("""jhon bought a new fast 4-wheel car. He daily drives this car but the car is disturbing him.""")
+# Modify tokenizer infix patterns
+infixes = (
+    LIST_ELLIPSES 
+    + LIST_ICONS
+    + [
+        r"(?<=[0-9])[+\\-\\*^](?=[0-9-])",
+        r"(?<=[{al}{q}])\\.(?=[{au}{q}])".format(
+            al=ALPHA_LOWER, au=ALPHA_UPPER, q=CONCAT_QUOTES
+        ),
+        r"(?<=[{a}]),(?=[{a}])".format(a=ALPHA),
+        # ✅ Commented out regex that splits on hyphens between letters:
+        # r"(?<=[{a}])(?:{h})(?=[{a}])".format(a=ALPHA, h=HYPHENS),
+        r"(?<=[{a}0-9])[:<>=/](?=[{a}])".format(a=ALPHA),
+    ]
+)
 
+infix_re = compile_infix_regex(infixes)
+nlp.tokenizer.infix_finditer = infix_re.finditer
 
 @app.route('/coreference_resolution', methods=['POST'])
 def coreference_resolution():
     # Get the input text from the request
-    input_text = request.json.get('text', '')
+    input_text = request.json.get('document', '')
 
     # Process the input text with spaCy
     doc = nlp(input_text)
